cbe195bd232c546394940151658c7160ec531ffb
Tomasz <98025536+TomaszSarmata@users.noreply.github.com>
2023-08-02 15:11:34 +0100

    created a parent Board component and Button child component. Moved the state up to the parent to update the values accordingly

--- src/App.js
+++ src/App.js
@@ -1,3 +1,65 @@
-export default function Square() {
-  return <button className="square">X</button>;
+import { useState } from "react";
+
+function Square({ value, onSquareClick }) {
+  return (
+    <button onClick={onSquareClick} className="square">
+      {value}
+    </button>
+  );
+}
+
+export default function Board() {
+  const [squares, setSquares] = useState(Array(9).fill(null));
+  function handleClick(i) {
+    const nextSquares = squares.slice();
+    nextSquares[i] = "X";
+    setSquares(nextSquares);
+  }
+
+  return (
+    <>
+      <div className="board-row">
+        <Square
+          onSquareClick={() => handleClick(0)}
+          value={squares[0]}
+        ></Square>
+        <Square
+          onSquareClick={() => handleClick(1)}
+          value={squares[1]}
+        ></Square>
+        <Square
+          onSquareClick={() => handleClick(2)}
+          value={squares[2]}
+        ></Square>
+      </div>
+      <div className="board-row">
+        <Square
+          onSquareClick={() => handleClick(3)}
+          value={squares[3]}
+        ></Square>
+        <Square
+          onSquareClick={() => handleClick(4)}
+          value={squares[4]}
+        ></Square>
+        <Square
+          onSquareClick={() => handleClick(5)}
+          value={squares[5]}
+        ></Square>
+      </div>
+      <div className="board-row">
+        <Square
+          onSquareClick={() => handleClick(6)}
+          value={squares[6]}
+        ></Square>
+        <Square
+          onSquareClick={() => handleClick(7)}
+          value={squares[7]}
+        ></Square>
+        <Square
+          onSquareClick={() => handleClick(8)}
+          value={squares[8]}
+        ></Square>
+      </div>
+    </>
+  );
 }
